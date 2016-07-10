0b3bf001e7e8ce6d10d31fb162a949e8ef8f8022
Jesú Márquez <jesumarquez@gmail.com>
2016-07-10 17:05:50 -0300

    watch nodemon

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-app.listen(3000, function(){
-    console.log('running...');
+app.listen(port, function(){
+    console.log('server is running on port ' + port + ' !!!');
 });
